37645630c3c764d25ca60347ec0c34e1d01073bc
Arshit Chaudhary <f20220331@pilani.bits-pilani.ac.in>
2025-10-22 01:33:29 +0530

    test supabase connection

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from auth.supabase_client import supabase
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return{"message": "Hello World"}
+
+@app.get("/health")
+def health():
+    try:
+        supabase.table("health-meta").select("id").limit(1).execute()
+        return {"status": "ok"}
+    except Exception as e:
+        return {"status": "fail", "error": str(e)}
